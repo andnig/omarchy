5e25b6f521c7d1e5f34353e4b466f566b1856b40
Andreas Nigg <andreas.nigg@devopsandmore.com>
2025-07-07 10:41:55 +0200

    remove stuff we don't need

--- install/xtras.sh
+++ install/xtras.sh
@@ -1,6 +1,6 @@
 yay -S --noconfirm --needed \
-  signal-desktop spotify dropbox-cli zoom \
-  obsidian-bin typora libreoffice obs-studio kdenlive \
+  signal-desktop spotify zoom \
+  libreoffice obs-studio kdenlive \
   pinta xournalpp
 
 # Copy over Omarchy applications
