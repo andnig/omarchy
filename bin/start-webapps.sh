db6bc70e62650fe5864b3667f869a548b776d121
Andreas Nigg <andreas.nigg@devopsandmore.com>
2025-07-19 22:31:14 +0200

    startup fixes

--- bin/start-webapps.sh
+++ bin/start-webapps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 hyprctl dispatch workspace 5
-
+sleep 0.5
 uwsm app -- chromium --user-data-dir='/home/andreas/.config/chromium' --profile-directory='Profile 2' --ozone-platform=wayland --app="https://chatgpt.com" &
 sleep 0.2
 uwsm app -- chromium --user-data-dir='/home/andreas/.config/chromium' --profile-directory='Profile 2' --ozone-platform=wayland --app="https://claude.com" &
