697eeb38f0a3330f8439e89fffcea107bcbe6f59
Andreas Nigg <andreas.nigg@devopsandmore.com>
2025-07-19 20:54:39 +0200

    fix webapps

--- bin/start-webapps.sh
+++ bin/start-webapps.sh
@@ -2,9 +2,8 @@
 
 hyprctl dispatch workspace 5
 
-chromium --user-data-dir='/home/andreas/.config/chromium' --profile-directory='Profile 2' --ozone-platform=wayland --app="https://chatgpt.com"
-chromium --user-data-dir='/home/andreas/.config/chromium' --profile-directory='Profile 2' --ozone-platform=wayland --app="https://claude.com"
-
+uwsm app -- chromium --user-data-dir='/home/andreas/.config/chromium' --profile-directory='Profile 2' --ozone-platform=wayland --app="https://chatgpt.com" &
 sleep 0.2
-
+uwsm app -- chromium --user-data-dir='/home/andreas/.config/chromium' --profile-directory='Profile 2' --ozone-platform=wayland --app="https://claude.com" &
+sleep 0.5
 hyprctl dispatch workspace previous
