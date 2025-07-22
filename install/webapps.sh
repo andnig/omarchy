20b80a4f109144aa1b1bd7e181225afce2e88f26
Andreas Nigg <andreas.nigg@devopsandmore.com>
2025-07-22 11:21:49 +0200

    a lot of stuff

--- install/webapps.sh
+++ install/webapps.sh
@@ -9,3 +9,4 @@ web2app "ChatGPT" https://chatgpt.com/ https://cdn.jsdelivr.net/gh/homarr-labs/d
 web2app "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
 web2app "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
 web2app "X" https://x.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/x-light.png
+web2app "Claude" https://claude.ai/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/claude-ai.png
